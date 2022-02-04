5a1bf09d24dc253b850e5de252cdeb3165a7851d
Rebecca Martinez <rebecca.m.martinez30@gmail.com>
2022-02-04 12:15:36 -0600

    Function Excercises

--- function_fun.js
+++ function_fun.js
@@ -1,26 +1,24 @@
 "use strict";
 
-tryGetRideShare( 20,true, getFundsAvailible());
+tryGetRideShare( 20,true, getFundsAvailible);
 
-function getFundsAvailible (){
+function getFundsAvailible(){
     let moneyInAccount = prompt ("How much is in the account");
-    let creditsInAccount= parseFloat("How much is in the account"));
+    let creditsInAccount= parseFloat("How much is in the account");
     return moneyInAccount + creditsInAccount;
 }
 
-    /*
     let numbOfRiders;
     if (confirm("Are you splitting the ride with someone else?")){
         numbOfRiders = prompt ("How many additional Riders do you have?");
     }
 
-}
 
 
 function tryGetRideShare(costOfRide, driverAvailable, totalFundsAvail){
 // don't know how much the ride is going to be -done
 // don't know if the driver is ready/near -done
-// money sources? -
+money sources? -
 // how much money is available -done
 
     if (driverAvailable && totalFundsAvail >= costOfRide){
@@ -33,4 +31,3 @@ function tryGetRideShare(costOfRide, driverAvailable, totalFundsAvail){
     }
 }
 
-*/
