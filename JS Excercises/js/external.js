776c82aa1abbb0574fd0b3fb3bbcdffa5b131f11
Rebecca Martinez <rebecca.m.martinez30@gmail.com>
2022-02-03 19:01:13 -0600

    Adding JS Excercises

---
+++ JS Excercises/js/external.js
@@ -0,0 +1,72 @@
+"use strict";
+console.log('Hello from JavaScript!');
+
+/*alert ("Welcome to my Website!");
+
+var userFavColor = prompt("What is your favorite color");
+console.log('The user entered: ' + userFavColor);*/
+
+/*var dailyRentalPrice = 3;
+
+var theLilMerm = Number(prompt("How long would you like to rent this Movie?")) * dailyRentalPrice;
+var broBear = Number(prompt("How long would you like to rent this Movie?")) * dailyRentalPrice;
+var hercules = Number(prompt("How long would you like to rent this Movie?")) * dailyRentalPrice;
+
+var total = theLilMerm + broBear + hercules;
+
+alert("Total amount for all movies " + total);*/
+
+
+/*var googleTime = Number(prompt ("How many hours worked at the Googs?")) * 400;
+var amazonTime = Number(prompt("How many hours did Bez make you work?")) * 380;
+var facebookTime = Number(prompt("How many hours did you spend in the meta?")) * 350;
+var totalpaid = googleTime + amazonTime + facebookTime
+
+alert("You got paid son!" +totalpaid);*/
+
+
+
+/*let maxClass = Number(prompt("What is the maximum number of students in the class?"));
+console.log (maxClass);
+let classSize = Number(prompt( "How many students are in the class currently?")) + 1;
+console.log(classSize);
+let doesItConflict = confirm( "Click Ok if this class does not conflict with any other classes.");
+console.log (doesItConflict);
+
+if (doesItConflict && (classSize <= maxClass)){
+    alert("You made the cut!");
+    console.log("Yay!");
+} else {
+    alert("We are so sorry, you didnt make the cut.");
+    console.log("Whomp Whomp Whomp");
+}*/
+
+
+
+/*let productMin= 2;
+let amountOfProducts = Number(prompt("How Many Items are in your cart?"));
+let expire = confirm("Is this offer expired");
+
+let premMbr = confirm("Press OK if you are a Premium Mbr");
+
+if (premMbr || !expire && amountOfProducts > productMin) {
+    alert("Congrats discount applied.");
+} else {
+    alert("Discount was not applied.");
+}*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
