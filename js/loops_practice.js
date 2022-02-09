11c31fdecdc17bbe0d1a69bc0d40925951ac7884
Rebecca Martinez <rebecca.m.martinez30@gmail.com>
2022-02-09 14:35:53 -0600

    loops_excersise

---
+++ js/loops_practice.js
@@ -0,0 +1,22 @@
+//practice w casey
+
+/*let isAwesome = true
+let incrementor = 0;
+if (true) {
+    console.log("Loop has ended");
+}
+
+while (incrementor >0 ){
+    console.log(incrementor --);
+}*/
+
+
+let toFifty = 8;
+
+if (true) {
+    console.log("We have reached 50!");
+}
+while (toFifty < 50){
+    console.log(toFifty ++)
+}
+
