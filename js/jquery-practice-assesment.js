a761d4fa11b696854db62c8500cca8c2e00a9d82
Rebecca Martinez <rebecca.m.martinez30@gmail.com>
2022-03-15 09:06:42 -0500

    last changes

---
+++ js/jquery-practice-assesment.js
@@ -0,0 +1,47 @@
+// Question 1: When the box with the id of dbl-click-box is double-clicked,
+// an alert should pop up with the text 'BOX DOUBLE-CLICKED!'
+
+$('.dbl-click-box').click(function (){
+    $('h3').dblclick();
+});
+
+
+
+// Question 2 - Clicking on the button with an id of dark-mode-btn toggles dark mode on and off.
+// HINT: changes should be applied to the body of the HTML.
+
+
+$(document).ready(function() {
+    $("button").dblclick (function () {
+    )};
+}
+
+
+
+// Question 3 - When the cursor hovers over the span with the id of uppercase (<span id="uppercase">),
+// take the text of that span and display it, in lowercase, in the span with the id of lowercase (<span id="lowercase">).
+
+
+
+
+
+
+
+// Question 4
+
+
+
+
+
+// Question 5
+
+
+
+
+// Question 6
+
+
+
+
+
+// Question 7
