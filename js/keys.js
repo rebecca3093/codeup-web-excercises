474313dbcc1794f1f68f9fe571ca9f9ee96c7f1f
Rebecca Martinez <rebecca.m.martinez30@gmail.com>
2022-03-17 16:57:31 -0500

    mapbox

--- js/keys.js
+++ js/keys.js
@@ -1,17 +1,3 @@
 const OMW_Key = "e8d484337e011c938a532cb8bb0c8ee0";
-
-
-
-// Replace ./data.json with your JSON feed
-fetch('https://ghibliapi.herokuapp.com')
-    .then((response) => {
-        return response.json()
-    })
-    .then((data) => {
-        // Work with JSON data here
-        console.log(data)
-    })
-    .catch((err) => {
-        // Do something for an error here
-    })
+const MAP_Key = "pk.eyJ1IjoicmViZWNjYTMwOTMiLCJhIjoiY2wwdmQ1OGViMTYzejNsb3A1am0yOW8ycCJ9.bxhR1Hy_y_sYGd8XNHLHlQ";
 
