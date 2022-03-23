837509f2c22a22b7f5faeeb83b0f960486f1bf9d
Rebecca Martinez <rebecca.m.martinez30@gmail.com>
2022-03-23 12:31:54 -0500

    es6

---
+++ js/map-filter-reduce.js
@@ -0,0 +1,90 @@
+"use strict";
+
+const users = [
+    {
+        id: 1,
+        name: 'ryan',
+        email: 'ryan@codeup.com',
+        languages: ['clojure', 'javascript'],
+        yearsOfExperience: 5
+    },
+    {
+        id: 2,
+        name: 'luis',
+        email: 'luis@codeup.com',
+        languages: ['java', 'scala', 'php'],
+        yearsOfExperience: 6
+    },
+    {
+        id: 3,
+        name: 'zach',
+        email: 'zach@codeup.com',
+        languages: ['javascript', 'bash'],
+        yearsOfExperience: 7
+    },
+    {
+        id: 4,
+        name: 'fernando',
+        email: 'fernando@codeup.com',
+        languages: ['java', 'php', 'sql'],
+        yearsOfExperience: 8
+    },
+    {
+        id: 5,
+        name: 'justin',
+        email: 'justin@codeup.com',
+        languages: ['html', 'css', 'javascript', 'php'],
+        yearsOfExperience: 9
+    }
+];
+
+
+
+let usersWithThreeOrMoreLanguages = users.filter(function (user) {
+    return user.languages.length >= 3;
+});
+
+usersWithThreeOrMoreLanguages.forEach(function (user){
+    console.log(email);
+});
+
+let emailAddresses = users.map((user) => users.email);
+
+emailAddresses.forEach(function(email){
+    console.log(email);
+})
+
+let totalYears = users.reduce ((totalYears, user) => {
+    return totalYears + users.yearsOfExperience;
+}, 0);
+
+let averageYearsExp = totalYears / users.length;
+
+console.log(averageYearsExp);
+
+let longestEmailAddress =  users.reduce((longest, user) => {
+    if (user.email.length > longest) {
+        longest = user.email;
+    }
+    return longest;
+}, '')
+
+
+let instructorString = users.reduce((resultString, user) => {
+    return resultString += user.name + ",";
+}, 'Your Instructors are: ');
+
+console.log(instructorString);
+
+let languages = users.reduce((listOfLanguages, user) => {
+    user.languages.forEach(function (lang){
+        if (!listOfLanguages.includes(lang)){
+            listOfLanguages.push(lang);
+        }
+    })
+    return listOfLanguages
+}, [])
+
+
+console.log(languages);
+
---
+++ lectures/es6-lecture.js
@@ -0,0 +1,11 @@
+
+
+const super_secret_string = 'password123';
+
+//When we change the value of a sting we change the value of the old string.
+
+super_secret_string = "new string boi";
+
+
+
+
