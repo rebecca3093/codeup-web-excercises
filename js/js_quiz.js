7d2322831ce1b642dcd92dbefd8d292094aca40c
Rebecca Martinez <rebecca.m.martinez30@gmail.com>
2022-04-15 00:36:00 -0500

    dom query

---
+++ js/js_quiz.js
@@ -0,0 +1,35 @@
+
+//Question 1
+
+/*if (true) {
+    let x = 5;
+}
+console.log(typeof x);*/
+
+// Question 2
+
+/*
+function sayHello(name) {
+    return 'Hello, ' + name + '!';
+}
+
+const sayHello = (name) => { 'Hello, ' + name + '!' }
+
+*/
+
+//Question 3
+
+/*const isDivisibleBy3 = n => n % 3 === 0;
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+console.log(numbers.filter(isDivisibleBy3));
+console.log(numbers);*/
+
+
+//question 6
+
+let numbers = [1, 2, 3, 4, 5];
+numbers.map(n => n * 3)
+
+console.log(numbers.map(n => n * 3));
